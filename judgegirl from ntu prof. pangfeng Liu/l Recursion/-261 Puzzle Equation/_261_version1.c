c29b6136c548cb6495ad77afb9350947b33976b9
Huang,Zhen-Zhang <107886703+huangzz02@users.noreply.github.com>
2025-09-24 03:52:19 +0800

    Add files via upload

---
+++ judgegirl from ntu prof. pangfeng Liu/l Recursion/-261 Puzzle Equation/_261_version1.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+void check(char *str1, char *str2, char *str3, int *val) {
+    char copy1[20], copy2[20], copy3[20];
+    strcpy(copy1, str1);
+    strcpy(copy2, str2);
+    strcpy(copy3, str3);
+
+    for (int i = 0; copy1[i] != '\0'; i++) {
+        if (isupper((unsigned char)copy1[i])) {
+            copy1[i] = '0' + val[copy1[i] - 'A'];
+        }
+    }
+    for (int i = 0; copy2[i] != '\0'; i++) {
+        if (isupper((unsigned char)copy2[i])) {
+            copy2[i] = '0' + val[copy2[i] - 'A'];
+        }
+    }
+    for (int i = 0; copy3[i] != '\0'; i++) {
+        if (isupper((unsigned char)copy3[i])) {
+            copy3[i] = '0' + val[copy3[i] - 'A'];
+        }
+    }
+
+    unsigned long long n1 = strtoull(copy1, NULL, 10);
+    unsigned long long n2 = strtoull(copy2, NULL, 10);
+    unsigned long long n3 = strtoull(copy3, NULL, 10);
+
+    if (n1 * n2 == n3) {
+        printf("%s x %s = %s\n", copy1, copy2, copy3);
+        exit(0);
+    }
+}
+
+void dfs(int pos, char *unique, int num_unique, int *val, char *str1, char *str2, char *str3, bool *digit_used) {
+    if (pos == num_unique) {
+        check(str1, str2, str3, val);
+        return;
+    }
+
+    for (int d = 0; d <= 9; d++) {
+        if (digit_used[d]) continue;
+
+        val[unique[pos] - 'A'] = d;
+        digit_used[d] = true;
+        dfs(pos + 1, unique, num_unique, val, str1, str2, str3, digit_used);
+        digit_used[d] = false;
+    }
+}
+
+void solve(char *str1, char *str2, char *str3) {
+    char unique[27];
+    bool used[26] = {false};
+    int num_unique = 0;
+
+    for (int i = 0; str1[i] != '\0'; i++) {
+        if (isupper((unsigned char)str1[i]) && !used[str1[i] - 'A']) {
+            used[str1[i] - 'A'] = true;
+        }
+    }
+    for (int i = 0; str2[i] != '\0'; i++) {
+        if (isupper((unsigned char)str2[i]) && !used[str2[i] - 'A']) {
+            used[str2[i] - 'A'] = true;
+        }
+    }
+    for (int i = 0; str3[i] != '\0'; i++) {
+        if (isupper((unsigned char)str3[i]) && !used[str3[i] - 'A']) {
+            used[str3[i] - 'A'] = true;
+        }
+    }
+    
+    for (int i = 0; i < 26; i++) {
+        if (used[i]) {
+            unique[num_unique++] = 'A' + i;
+        }
+    }
+    /*
+        Example: input "ATHF FHTA 5HHTAAF",
+                 -> unique = ['A','T','H','F'], num_unique = 4.
+    */
+
+    int val[26] = {0};
+    bool digit_used[10] = {false};
+
+    dfs(0, unique, num_unique, val, str1, str2, str3, digit_used);
+}
+
+int main() {
+    char str1[20], str2[20], str3[20];
+    scanf("%s", str1);
+    scanf("%s", str2);
+    scanf("%s", str3);
+    solve(str1, str2, str3);
+    return 0;
+}
+
+/*
+    This program should be the most complete version,
+    but because the problem is simplified in difficulty,
+    it allows different letters to use the same number,
+    so this version may not be applicable.
+
+    example input:
+    AB CD 242
+    expected output (in this question):
+    22 x 11 = 242
+    actual output:
+    (no output)
+*/
